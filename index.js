2ccccc3522a6156f6d9a410a9c6ab268b819b620
Ashutosh lakshakar <lakshakarashutosh@gmail.com>
2024-04-26 10:24:54 +0530

    feat: Added new updated code

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const { ACCESS_KEY, SECRET_KEY, BUCKET_NAME, REGION, PORT } = process.env;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin:"*"
+}));
 
 app.get("/health", (req, res) => {
   console.log("OK");
@@ -18,38 +21,88 @@ const S3 = require("aws-sdk/clients/s3");
 const { randomUUID } = require("crypto");
 
 const s3 = new S3({
-  apiVersion: "2006-03-01",
   accessKeyId: ACCESS_KEY,
   secretAccessKey: SECRET_KEY,
   region: REGION,
   signatureVersion: "v4",
 });
 
+
+// app.post("/getPresignUrl", async (req, res) => {
+//   try {
+//     const fileType = req.body.fileType || "";
+
+//     const ex = fileType.split(".")[1];
+//     console.log(ex);
+//     const Key = `${randomUUID()}.${ex}`;
+//     const s3Params = {
+//       Bucket: BUCKET_NAME,
+//       Key,
+//       Expires: 600,
+//       ContentType: `image/${ex}`,
+//     };
+//     const uploadUrl = await s3.getSignedUrl("putObject", s3Params);
+
+//     console.log("uploadUrl ***********************", uploadUrl);
+//     res.status(200).json({
+//       uploadUrl,
+//       key: Key,
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       error: error.message,
+//     });
+//   }
+// });
+
+
+
 app.post("/getPresignUrl", async (req, res) => {
   try {
-    const fileType = req.query.fileType || "";
+    if (
+      !process.env.ACCESS_KEY ||
+      !process.env.SECRET_KEY ||
+      !process.env.REGION
+    ) {
+      throw {
+        message: "AWS credential is missing",
+        statusCode: 500,
+      };
+    }
+
+    const s3 = new S3({
+      apiVersion: "2006-03-01",
+      accessKeyId: process.env.ACCESS_KEY,
+      secretAccessKey: process.env.SECRET_KEY,
+      region: process.env.REGION,
+      signatureVersion: "v4",
+    });
+
+    const { fileType } = req.body;
     const ex = fileType.split(".")[1];
-    console.log(ex);
-    const Key = `${randomUUID()}.${ex}`;
+    const Key = `user/ashutosh/${randomUUID()}.${ex}`;
+
     const s3Params = {
-      Bucket: BUCKET_NAME,
+      Bucket: process.env.BUCKET_NAME,
       Key,
       Expires: 600,
       ContentType: `image/${ex}`,
     };
+
     const uploadUrl = await s3.getSignedUrl("putObject", s3Params);
 
-    // console.log("uploadUrl ***********************", uploadUrl);
     res.status(200).json({
       uploadUrl,
       key: Key,
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(500).json({
       error: error.message,
     });
   }
 });
+
+
 
 app.delete("/delete", async (req, res) => {
   try {
